4940b0aaf8d931e495a1f6f1492982d42bc6c178
E891-me <mesoma.onyelusi@pau.edu.ng>
2025-10-20 15:39:48 +0100

    My week-4 practice_4

---
+++ week-4/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\pc\Desktop\m.onyelusiCOS101\m.onyelusiCOS101\week-4\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\pc\Desktop\m.onyelusiCOS101\m.onyelusiCOS101\week-4\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
