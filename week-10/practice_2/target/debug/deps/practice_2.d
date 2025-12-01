48de5235411ed61cf7823dde097175302c57a511
Onyelusi Mesoma <mesoma.onyelusi@pau.edu.ng>
2025-12-01 04:37:29 +0100

    My week-10 practices

---
+++ week-10/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\DELL\Desktop\m.onyelusiCOS101\week-10\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\DELL\Desktop\m.onyelusiCOS101\week-10\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\DELL\Desktop\m.onyelusiCOS101\week-10\practice_7\target\debug\practice_7.exe: C:\Users\DELL\Desktop\m.onyelusiCOS101\week-10\practice_7\src\main.rs
